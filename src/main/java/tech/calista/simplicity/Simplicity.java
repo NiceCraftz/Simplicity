647f743b42442985e128ae652eea2b284528fba1
Alessandro <alessandromaria.calista@gmail.com>
2022-09-18 22:48:19 +0200

    Initial commit

---
+++ src/main/java/tech/calista/simplicity/Simplicity.java
@@ -0,0 +1,17 @@
+package tech.calista.simplicity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Simplicity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
